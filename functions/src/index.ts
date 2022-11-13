6554e919ee0cb5e8b422efdd4a34e7d02cc4fcb1
Tyler Stark <theirondeveloper@gmail.com>
2022-11-13 16:00:01 -0600

    Setup webhook endpoint and strategy extraction

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,29 @@
-import * as functions from "firebase-functions";
+import {logger} from "firebase-functions";
+import {https} from "firebase-functions/v2";
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+enum Strategy {
+  UNKNOWN= "unknown",
+  GITHUB = "github",
+  CLI = "cli"
+}
+
+export const webhook = https.onRequest((req, resp) => {
+  const strategy = getStrategy(req);
+  logger.info("Hello logs!", {strategy});
+  resp.json({status: 200, strategy});
+});
+
+/**
+ * @param {https.Request} req https request handler
+ * @return {Strategy} derived from request
+ */
+function getStrategy(req: https.Request): Strategy {
+  switch (req.query.sp) {
+    case "cli":
+      return Strategy.CLI;
+    case "github":
+      return Strategy.GITHUB;
+    default:
+      return Strategy.UNKNOWN;
+  }
+}
